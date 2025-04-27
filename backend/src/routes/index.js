ce421bd04d4a32a3faf920d2d6b2a543fb802b3a
ANUKULUNJ <anukuljain42@gmail.com>
2025-04-28 02:26:35 +0530

    er2

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,20 +1,11 @@
-import express from 'express';
-import userAuthRoutes from './userAuthRoutes.js';
-import userRoutes from './userRoutes.js';
-import expertRoutes from './expertRoutes.js';
-import blogRoutes from './blogRoutes.js';
-import communityRoutes from './communityRoutes.js';
-import sessionRoutes from './sessionRoutes.js';
-import assessmentRoutes from './assessmentRoutes.js';
+import express from "express";
+import userRouter from "./userRoutes.js";
+import expertRouter from "./expertRoutes.js";
 
 const router = express.Router();
 
-router.use('/auth', userAuthRoutes);
-router.use('/users', userRoutes);
-router.use('/experts', expertRoutes);
-router.use('/blogs', blogRoutes);
-router.use('/community', communityRoutes);
-router.use('/sessions', sessionRoutes);
-router.use('/assessment', assessmentRoutes);
+router.use("/user", userRouter);
+router.use("/expert", expertRouter);   
+
 
 export default router;
