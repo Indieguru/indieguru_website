ede7629cef8c3a341ef5d1cfd050452b7d9f5671
AdiNarang01 <narang.adi01@gmail.com>
2025-10-27 20:17:25 +0530

    corsOptionsReverted

--- backend/server.js
+++ backend/server.js
@@ -28,9 +28,7 @@ const corsOptions = {
       ? `${process.env.FRONTEND_URL}:${process.env.FRONTEND_PORT}`
       : process.env.FRONTEND_URL,
     'http://localhost:5173',
-    'http://localhost:3000',
-    'https://myindieguru.com/',
-    'https://www.myindieguru.com/'
+    'http://localhost:3000'
   ],
   credentials: true,
 };
